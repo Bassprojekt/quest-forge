4007daa5cc3bc186332c42ed43739ed5377fc219
Developer <dev@example.com>
2026-04-15 18:50:04 +0200

    fix: adjust dragon eyes position

--- src/components/game/PetModels.tsx
+++ src/components/game/PetModels.tsx
@@ -60,7 +60,7 @@ const DragonModel = ({ color, evolved }: { color: string, evolved: boolean }) =>
       <boxGeometry args={[0.3, 0.3, 0.3]} />
       <meshStandardMaterial color={color} />
     </mesh>
-    <Eyes position={[0, 0.5, 0.45]} />
+    <Eyes position={[0, 0.52, 0.55]} />
     <mesh position={[0.5, 0.2, -0.2]} rotation={[0, 0, 0.5]}>
       <boxGeometry args={[0.7, 0.05, 0.3]} />
       <meshStandardMaterial color="#999" />
